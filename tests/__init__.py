68976c111f5f0e9d69f962b9aa3edf147f1807b8
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-03-29 09:32:51 +0100

    Moving tests to their own directory

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from django.conf import settings
-if not settings.configured:
-    settings.configure(DEBUG=True)
---
+++ tests/django/__init__.py
@@ -0,0 +1,3 @@
+from django.conf import settings
+if not settings.configured:
+    settings.configure(DEBUG=True)
